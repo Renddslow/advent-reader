23a47163cbadafed801918d243af45bf39f79728
Matt McElwee <mmcelwee@dmsi.com>
2021-11-28 20:43:52 -0600

    Added intros for day 2

--- src/Reading/intros/02-evening.ts
+++ src/Reading/intros/02-evening.ts
@@ -1,1 +1,7 @@
-export default ``;
+export default `
+There is much to pay attention to at the close of Genesis. On one hand, there is the story of Joseph, a beautiful novella which sets the stage for many later "faithful Jew" stories like Daniel and Esther. It also demonstrates the faithfulness of YHWH to preserve the covenant made with Abraham and to bless the whole world.
+
+The theme of blessing is important to these chapters. Pay attention to the way the word is used and how it has been used prior to this section throughout the book of Genesis. How does God enact blessing differently from the way the human characters enact blessing? You'll start to see foreshadowing of the blessing promised to the whole world.
+
+You'll also notice that Joseph seems to match many of the characteristic of the snake crusher and the covenant promise made to Abraham. He suffers under evil, but ultimately restores the whole world – giving food during a time of great famine. Perhaps we are seeing the blessing meant for the whole world.
+`;
--- src/Reading/intros/02-morning.ts
+++ src/Reading/intros/02-morning.ts
@@ -1,1 +1,7 @@
-export default ``;
+export default `
+God promised the woman a son who would conquer the serpent and set the world to rights. God later promised Abraham that this son would come through Abraham's family. The hint we have been given is to pay close attention to the offspring of those connected to Abraham – which is why genealogy is such a common genre throughout the early part of the _Torah_.
+
+Last night we read about Abraham's death. While chronologically his death follows many years after the binding of Isaac in Genesis 22, within the narrative, Abraham all but passes on as a character in the proceeding chapters. The binding of Isaac is Abraham's climax. In that triumphant, yet equally terrible moment, Abraham demonstrated a fidelity and trust in YHWH which had alluded him his entire life. Now, with the saga moved to his son, Isaac, and his grandson, Jacob, we will watch to see if this same fidelity will be passed on.
+
+Jacob takes up a major space of the narrative, and is important to watch. We're presented with a trickster archetype who outwits his enemies and even wrestles with the angel of YHWH and wins. We are left to wonder, will this twisted and troubled anti-hero be the serpent crusher?
+`;
