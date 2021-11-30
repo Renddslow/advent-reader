89e7d9883387495161571fdbacf15f8d6f08c070
Matt McElwee <mmcelwee@dmsi.com>
2021-11-29 22:30:24 -0600

    Added an intro for 3/evening

--- src/Reading/intros/03-evening.ts
+++ src/Reading/intros/03-evening.ts
@@ -1,1 +1,12 @@
-export default ``;
+export default `
+YHWH contrasts the powers of Pharaoh in his ability to deliver his people. Pharaoh self-describes as one with a mighty hand and an outstretched arm. His power is from the might of his armies, his chariots and his horses. But YHWH turns the whole cosmos in on Pharaoh and his kingdom.
+
+At the last, YHWH shows that even with a small body of water he can best the might of Egypt – the horse and the chariot. The song of victory Moses sings speaks to God as a warrior and a deliver, the day of the Lord visited against Egypt. This imagery builds on what we already know about the snake crusher:
+
+1. He will be a son of the woman
+2. He will come from Abraham's family and bless the whole world
+3. He will be a king
+4. He will be a warrior.
+
+We may be tempted to view Moses as the snake crusher for the briefest of moments, but we quickly realize that in today's readings, YHWH is the hero of the story. Moses is a mere servant.
+`;
