5a5f7f06a490683f43f5686875b364e537542c90
Matt McElwee <mmcelwee@dmsi.com>
2021-12-01 00:41:03 -0600

    Added intros for day 4

--- src/Reading/intros/04-evening.ts
+++ src/Reading/intros/04-evening.ts
@@ -1,1 +1,9 @@
-export default ``;
+export default `
+God has always wanted to dwell among his people. We saw in Genesis 3 that God walked in the garden in the cool of the day, as though it were his regular custom. The nature of human rebellion has left the people ritually unclean, which is dangerous in the presence of almighty God. So in order to dwell with his people, God must create a clean space among his people where he can dwell.
+
+This section of Exodus may feel anti-climactic to us as modern readers, especially when compared to the Reed Sea incident and the moving denouement of Moses' song of the sea. But in fact, YHWH coming to inhabit the Tabernacle is the real climax of the Exodus storyline, and brings a small sense of closure to the conflict begun in Genesis 3. 
+
+As we see throughout the prescriptions of the Tabernacle's furnishings and architecture, this space is designed to reflect God's heavenly throne room. The images of cherubim throughout the space are physical representations of the real spiritual beings who guard God's throne and worship him endlessly (we'll see them again in Isaiah, Ezekiel, and Revelation). But the Garden of Eden was also designed to reflect God's heavenly throne room. These were unique, sacramental spaces where heaven and earth overlapped and God could be among his people.
+
+While this may seem laborious to read through building instructions, immerse yourself in the sights, the sounds, and the smells of the Tabernacle as it is so vividly described and imagine what it would be like to be in God's earthly throne room.   
+`;
--- src/Reading/intros/04-morning.ts
+++ src/Reading/intros/04-morning.ts
@@ -1,1 +1,5 @@
-export default ``;
+export default `
+YHWH saves the people of Israel, his chosen people, from bondage. He brings them out of Egypt, saves them from military incursion, feeds them, and gives them clean water to drink. All of this is done as an act of grace – purely unmerited. The people of Israel have been shown the goodness of YHWH and his great power. They are now given a choice: live as vassals under YHWH's good rule, or go their own way and become just like the other nations.
+
+What follows throughout most of the _Torah_ is God's instruction to his people on how to live as people under his rule. While blessings and curses are associated with keeping or breaking his instruction, they are by no means saved on their own merit. God has already saved them.
+`;
