2a1d837690674cbf10ffc140ad283638f31c1c8e
Matt McElwee <mmcelwee@dmsi.com>
2021-12-01 23:10:04 -0600

    Added intro for 5/evening

--- src/Reading/intros/05-evening.ts
+++ src/Reading/intros/05-evening.ts
@@ -1,1 +1,5 @@
-export default ``;
+export default `
+It is often at this part of the _Torah_ that we may be tempted to lose the thread of the Bible as a unified story that leads to Jesus. How can any of this make sense in light of the Messiah? Some have taken a negative approach. Perhaps this exists to show just how bad the old system was. Of course, this is troublesome, as this system is created and ordained by God. On the other hand, some might suggest that it has nothing at all to do with Jesus, since he has superseded the Levitical codes.
+
+But there is a figure who appears throughout Leviticus that points us to Jesus: the high priest. Pay attention to the description of the high priest and to the importance of the day of atonement. These will become vitally important when we start reading the New Testament.
+`;
