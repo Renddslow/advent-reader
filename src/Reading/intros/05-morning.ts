baec0a1aa747cad2148c88054b85bc1080291406
Matt McElwee <mmcelwee@dmsi.com>
2021-12-01 23:06:45 -0600

    Added intro for day 5 morning

--- src/Reading/intros/05-morning.ts
+++ src/Reading/intros/05-morning.ts
@@ -1,1 +1,5 @@
-export default ``;
+export default `
+As modern readers it can be hard to engage with the text of Leviticus. It often occurs to us as being archaic and out-dated. To some it may even feel superstitious and repetitive. Yet the role of sacrifice and ritual purity were vitally important to the people of YHWH. The distinction of holy from profane was about more than just good worship, but even more, to the very ordering of the cosmos God had ordained.
+
+We must approach these texts as much as we can with the wonder and importance that the original hearers would have. How wonderful it was that through the ordinary life fluids of created things a holy space could be me made through which grace can flow and God can be with his people.
+`;
