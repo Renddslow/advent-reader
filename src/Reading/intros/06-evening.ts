50d13ee064d8936351b0b97094603a8ccd0551aa
Matt McElwee <mmcelwee@dmsi.com>
2021-12-03 22:58:22 -0600

    Added content for day 6

--- src/Reading/intros/06-evening.ts
+++ src/Reading/intros/06-evening.ts
@@ -1,1 +1,5 @@
-export default ``;
+export default `
+Deuteronomy is easily one of the most exciting books in the _Torah_. At first you might be thinking, what? Why? Isn't this just more of the same thing we've gotten before? It starts with a recap of the story so far and then dives right into more laws. But in fact, Deuteronomy is a deeply nuanced look at the civilization of Israel, their worship of God once they enter the promised land, and the important roles of the leaders of the community.
+
+Today's reading in Deuteronomy sets up an important precedent: there will be only one Temple in Israel. This is for one, quite unusual for many ancient cults. But it also sets up several conflicts both in the Hebrew Bible and during the life of Jesus. Pay attention to the importance of the worship of God in the community.  
+`;
--- src/Reading/intros/06-morning.ts
+++ src/Reading/intros/06-morning.ts
@@ -1,1 +1,5 @@
-export default ``;
+export default `
+It's common to expect human deliverers throughout the story of Scripture. We think about Sunday school stories of Joshua, Samson, and David. But what we really see time and again is God delivering his people, often through unconventional means. 
+
+In this morning's readings we'll read about two events which build upon one another to show the people of Israel rebelling against God in the wilderness. But despite their stubbornness, YHWH fights for them. When serpents attack the camp, God provides a means of healing by looking upon the bronze snake that was lifted up. And when a foreign shaman tries to curse the nation of Israel, God compels him to only speak blessing. YHWH uses untraditional symbols – symbols associated with enemies and chaos – to extend grace and blessing.
+`;
