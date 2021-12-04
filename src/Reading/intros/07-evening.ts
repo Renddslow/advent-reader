6e947dc303fe02fc8b3b2fa567e99e546bff3225
Matt McElwee <mmcelwee@dmsi.com>
2021-12-04 08:53:59 -0600

    Added content for day 7

--- src/Reading/intros/07-evening.ts
+++ src/Reading/intros/07-evening.ts
@@ -1,1 +1,5 @@
-export default ``;
+export default `
+There's a real poetic symmetry to the end of Deuteronomy in light of the rest of the _Torah_. Like Genesis, it ends with a series of blessings for the children of Israel. Like Leviticus, it ends with a series of covenant curses associated with violating the instructions of God. This section is beautiful in that it is deeply prophetic. It is prophetic in two senses of the word. On one hand, it is prophetic in the classical Biblical sense: calling the people to return to God. On the other, it also lays down a series of future-tellings that will be revealed in the later narratives and in Jesus.
+
+An important thing to pay attention to in these curses is how these curses were most commonly applied to the people in the later narratives due to the failure of priests, prophets, and kings in their capacity as representatives of the people.
+`;
--- src/Reading/intros/07-morning.ts
+++ src/Reading/intros/07-morning.ts
@@ -1,1 +1,5 @@
-export default ``;
+export default `
+Now I acknowledge that you may feel a bit bogged down at this point. It's your fourth day in a row reading about more instructions for God's covenant people. Why couldn't we skip over this like we had for Numbers? Well for starters, Deuteronomy lays much of the groundwork for what we'll see come out of the people of Israel over the next several narrative books. Many of the instructions given in this book are explicitly ignored and are the source of severe conflict. We also see Jesus quote from Deuteronomy heavily, in fact, the instructions of Deuteronomy are what Jesus combats the Devil with during his temptation in Matthew 4.
+
+As you're reading, pay attention to the rules for prophets, kings, and priests. How are those people meant to serve the nation of Israel, and how are they to be different than the other nations.
+`;
