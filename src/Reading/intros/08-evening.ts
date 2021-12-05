367835a9c1ad6b65eda9efa6913103f8bafb28d2
Matt McElwee <mmcelwee@dmsi.com>
2021-12-04 23:20:55 -0600

    Added content for day 8

--- src/Reading/intros/08-evening.ts
+++ src/Reading/intros/08-evening.ts
@@ -1,1 +1,5 @@
-export default ``;
+export default `
+After Joshua's death, the nation of Israel immediately insulates into their own tribes with various elders and tribal leaders coming to the fore to lead. Joshua's solemn declaration that he and his house will serve YHWH echoes only out into a few generations. Judges begins with the clean up work of Joshua's campaigns with the next generation rising up to take on giants of their own. But quickly the story shifts to a new type of deliverer. 
+
+These deliverers – judges – manage to be relatively godly and upright in this evening's reading, but tomorrow morning we'll see just how morally compromised these people are. Many abuse their God-given – and in one instance supernatural – power to gain wealth, prestige, and more power. This leaves the people wondering if a deliverer will ever come who is like one after God's own heart. What the people hope for is a king.
+`;
--- src/Reading/intros/08-morning.ts
+++ src/Reading/intros/08-morning.ts
@@ -1,1 +1,5 @@
-export default ``;
+export default `
+In Joshua we see the prototypical Israelite leader – think Deuteronomy 17. He meditates on God's _torah_ day and night, and affirms a life-long commitment to YHWH for himself and his household. It is common to reach for a connection to Jesus in Joshua. Certainly they share a name and a general disposition that it is YHWH who saves. But ultimately, Joshua is a very different kind of leader, and he isn't a deliverer. 
+
+Nevertheless, we see the story progress, with God fighting on behalf of his people, even getting creative when he is put in compromising positions.
+`;
