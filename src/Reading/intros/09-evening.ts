32e40bfb8e99970e8f51d20ccdc21fd1f1433c80
Matt McElwee <mmcelwee@dmsi.com>
2021-12-05 23:46:43 -0600

    Added intros for day 9

--- src/Reading/intros/09-evening.ts
+++ src/Reading/intros/09-evening.ts
@@ -1,1 +1,5 @@
-export default ``;
+export default `
+This morning's readings were difficult to watch. We left the heroes we'd found in Othniel, Ehud, and Deborah and wandered into the corrupt and compromised Gideon, Abimelech, and Jephthah. But this evening, we encounter the pinnacle of compromise in Samson. Samson compounds the violence of Abimelech and Gideon with the _torah_ ignorance of Jephthah. He is arrogant, vindictive, and a total womanizer. This guy is not the Bible's Superman. Samson's downfall is tragic and his epithet proves just how wasted his life truly was.
+
+The book of Judges concludes in even greater tragedy than before, with idolatry, corruption, theft, rape, and civil war. The nation of Israel has become far worse and far more corrupt than the Canaanites they had previous driven out. The writer of Judges pleads alongside the people for a king who would teach the people right from wrong. A king as described in Deuteronomy 17. A king who could be a priest and prophet as well.
+`;
--- src/Reading/intros/09-morning.ts
+++ src/Reading/intros/09-morning.ts
@@ -1,1 +1,9 @@
-export default ``;
+export default `
+Where yesterday we saw men and women of God rise up to deliver Israel, today we'll see men who become drunk on their authority and power and who don't truly know YHWH. 
+
+In the story of Gideon, we see a young man attempt a strange form of divination to draw out a victory from a God who'd already promised that same victory. Following a miraculous victory, Gideon seizes power, setting up a short-lived dynasty with himself at the forefront. And just as he'd relied on divination to win his victory, he set up an ephod – a sacramental and holy device in the context of the Levitical cult – as yet another form of divination and idolatrous worship. 
+
+In the story of Jephthah, we see a man wholly unfamiliar with the _torah_ of YHWH. First off, Jephthah makes a foolish oath, a thing explicitly warned against. Yet God offers escape from foolish vows (see Leviticus 5). Nevertheless, Jephthah believes that his success is because of a vow and not because of YHWH's faithfulness. And so when the fulfillment of his vow will mean offering his daughter as a burnt offering, he keeps his vow. Yet similar to the foolish vow, this practice is explicitly forbidden. YHWH finds offering humans – especially children – abhorrent.
+
+We see here not only a failure of God's chosen, human deliverers, but a failure of his chosen priests. The priests were tasked with teaching the people to remain faithful to YHWH. If only a priest-deliverer could rise up again like Moses and teach the people.
+`;
